a51aa82743e43543b0ce9fd5bd6cfff4e372263d
Florian Nagel <hi@floriannagel.xyz>
2019-11-23 04:45:47 +0100

    Add latest util aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,14 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias k="kubectl" # Kubernetes CLI Tool
+
+# Various useful utility commands
+alias brewup="brew update && brew doctor && brew outdated && brew upgrade && brew cleanup"
+alias uuid="python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)' | pbcopy && pbpaste && echo"
+
+# Enable Docker aliases
+if [ -f ~/.docker_aliases ]; then
+    . ~/.docker_aliases
+fi
